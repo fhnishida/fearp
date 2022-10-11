74ea0527d7353396e9f467aa0e178d73a5b0b7ad
fhnishida <43714027+fhnishida@users.noreply.github.com>
2022-10-10 23:49:01 -0300

    Add files via upload

--- rec2301/monitoria_06.R
+++ rec2301/monitoria_06.R
@@ -1,121 +1,108 @@
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-# MONITORIA 6 (06/10/2022)
+# MONITORIA 6
 
-# DISTRIB. NORMAL PADRÃO
-dnorm(-1.96, mean=0, sd=1)
+# DISTRIBUIÇÕES
+
+# NORMAL PADRÃO
 dnorm(1.96, mean=0, sd=1)
+dnorm(-1.96, mean=0, sd=1)
 
-pnorm(-1.96, mean=0, sd=1)
 pnorm(1.96, mean=0, sd=1)
+pnorm(-1.96, mean=0, sd=1)
 
-qnorm(0.025, mean=0, sd=1)
+pnorm(1.96, mean=0, sd=1) - pnorm(-1.96, mean=0, sd=1)
+
 qnorm(0.975, mean=0, sd=1)
+qnorm(0.025, mean=0, sd=1)
+
+# Gráfico fdp
+curve( dnorm(x, mean=0, sd=1), from=-3, to=3 )
+curve( pnorm(x, mean=0, sd=1), from=-3, to=3 )
+curve( qnorm(x, mean=0, sd=1), from=0, to=1 )
 
 
-# Gráfico com distrib. normal padrão
-Z = seq(-3.5, 3.5, by=0.1)
-probs = seq(0.001, 0.999, by=0.001)
+# T-STUDENT
+curve( dnorm(x, mean=0, sd=1), from=-3, to=3 )
 
-# Calcular as densidades, prob acumuladas e escores padrão
-pdf_norm = dnorm(Z, mean=0, sd=1)
-cdf_norm = pnorm(Z, mean=0, sd=1)
-qt_norm = qnorm(probs, mean=0, sd=1)
+curve( dt(x, df=100), from=-3, to=3, add=TRUE, col="red" )
 
 
-plot(Z, pdf_norm, type="l") # função de densidade de probabilidade
-plot(Z, cdf_norm, type="l") # função de distrb acumulada
-plot(probs, qt_norm, type="l") # retorna escore Z a partir de probs
+# AMOSTRAGEM ALEATÓRIA
+sample(1:10)
+sample(letters, 5)
 
 
-
-# AMOSTRAGEM
-sample(1:10, replace=T)
-
-# Visualização da LGN
+# Exemplo: Visualização da LGN
 sample(1:6, 1)
 
-amostra = replicate(500, expr=sample(1:6, 1))
-plot(table(amostra))
+amostra = replicate(500, exp=sample(1:6, 1))
+table(amostra)
 
-# sorteio de 2 números e cálculo da média
-mean(sample(1:6, 2, replace=T))
-
-amostra = replicate(500, expr=mean(sample(1:6, 2, replace=T)))
-plot(table(amostra))
-
-# sorteio de X números e cálculo da média
-X = 1
-mean(sample(1:6, X, replace=T))
-
-amostra = replicate(500, expr=mean(sample(1:6, X, replace=T)))
-plot(table(amostra), xlim=c(1, 6))
+plot(table(amostra), type="h")
 
 
-# Geração de números aleatórios
-rnorm(10, mean=0, sd=1)
+## 2 vezes o dado e fazer média
+mean( sample(1:6, 2, replace=TRUE) )
+
+amostra = replicate(500, exp=mean( sample(1:6, 2, replace=T) ))
+table(amostra)
+
+plot(table(amostra), type="h")
+
+
+## N vezes o dado e fazer média
+N = 100000
+
+mean( sample(1:6, N, replace=TRUE) )
+amostra = replicate(500, exp=mean( sample(1:6, N, replace=T) ))
+table(amostra)
+
+plot(table(amostra), type="h", xlim=c(1, 6))
+
+
+# GERAÇÃO DE NÚMEROS ALEATÓRIOS
+rnorm(5)
 
 set.seed(2)
-rnorm(10, mean=0, sd=1)
-
-# EXEMPLO 1
-a0 = 50
-b0 = -5
-N = 200
-
-set.seed(1)
-e = rnorm(N, 0, 2)
-x = rnorm(N, 5, 0.5)
-
-y = a0 + b0*x + e
-
-lm(y ~ x)
+rnorm(5)
 
 
-# EXEMPLO 2
-a0 = 50
-b0 = -5
-c0 = 3
-N = 200
+# EXEMPLO C.2
+SR87 = c(10, 1, 6, .45, 1.25, 1.3, 1.06, 3, 8.18, 1.67, .98,
+         1, .45, 5.03, 8, 9, 18, .28, 7, 3.97)
+SR88 = c(3, 1, 5, .5, 1.54, 1.5, .8, 2, .67, 1.17, .51, .5, 
+         .61, 6.7, 4, 7, 19, .2, 5, 3.83)
 
-set.seed(1)
-e = rnorm(N, 0, 2)
-x = rnorm(N, 5, 0.5)
-z = rnorm(N, 1.91, 0.25)
+Change = SR88 - SR87
+1 = length(Change)
+avgChange = mean(Change)
+sdChange = sd(Change)
 
-y = a0 + b0*x + c0*z + e
+# erro padrão
+se = sdChange / sqrt(n)
+CV = qt(.975, df=n-1)
 
-lm(y ~ x)
-lm(y ~ x + z)
+# limite inferior
+avgChange - CV*se
 
+# limite superior
+avgChange + CV*se
 
-# EXEMPLO 3
-a0 = 50
-b0 = -5
-c0 = 3
-N = 200
-
-set.seed(1)
-e = rnorm(N, 0, 2)
-x = rnorm(N, 5, 0.5)
-z = rnorm(N, 1.91*x, 0.25*x)
-
-y = a0 + b0*x + c0*z + e
-
-lm(y ~ x)
-lm(y ~ x + z)
+avgChange + CV * c(-se, se)
 
 
-# EXEMPLO 4
-a0 = 50
-b0 = -5
-N = 200
+# Exemplo C.6
+t = (avgChange - 0) / se
+CV
 
-set.seed(1)
-e = rnorm(N, 10, 2)
-x = rnorm(N, 5, 0.5)
+abs(t) > CV
 
-y = a0 + b0*x + e
-
-lm(y ~ x)
+p = 2 * (1 - pt(abs(t), n-1))
 
 
+# Calcular via t.test()
+testresults = t.test(Change)
+names(testresults)
+
+testresults$p.value
+testresults$estimate
